e33e568b8e0fbd1f6b176f9a4a9add76f5cb8edc
Jonathan <jonathanvm13@gmail.com>
2020-07-25 19:19:51 -0500

    trave restrictions input-output

---
+++ facebook-hacker-cup/2020/travel-restrictions/travel-restrictions.py
@@ -0,0 +1,16 @@
+import statistics
+import math
+import datetime
+import collections
+import sys
+
+def get_string(): return sys.stdin.readline().strip()
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
